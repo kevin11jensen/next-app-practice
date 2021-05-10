0c2560b6ef1049f2b25a0180ea603aa1832c6606
Kevin Jensen <kevinjensen1133@yahoo.com>
2021-05-10 12:14:58 -0700

    changed background color to pink with styled jsx

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,14 @@ export default function Index() {
             <h1>
                 Hello Nadine Happy Belated Mothers Day Now Lets Sell Some Damn Shirts!
             </h1>
+            <p>Your son is very intelligent and knows how to make websites that we can sell stuff on. I don't pretend I am a web developer like some folks at City Liquidators! I am actually a real life web developer that has experience!</p>
             <p>Love,<br/>Your emotionally abusive son</p>
+            <styled jsx>{`
+                div {
+                    height: 100vh;
+                    background-color: pink;
+                }
+            `}</styled>
         </div>
     )
 }
