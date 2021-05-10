1f63907ab4a5a92f508f80d13bc82bbecee9cbd2
Kevin Jensen <kevinjensen1133@yahoo.com>
2021-05-10 12:20:24 -0700

    fixed syntax error for style jsx, not styled jsx

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,14 @@ export default function Index() {
             </h1>
             <p>Your son is very intelligent and knows how to make websites that we can sell stuff on. I don't pretend I am a web developer like some folks at City Liquidators! I am actually a real life web developer that has experience!</p>
             <p>Love,<br/>Your emotionally abusive son</p>
-            <styled jsx>
+            <style jsx>
                 {`
                     .container {
                         height: 100vh;
                         background-color: pink;
                     }
                 `}
-            </styled>
+            </style>
         </div>
     )
 }
