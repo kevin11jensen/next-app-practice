077d3f92a293b76084760e1a09e382ff8f27d71a
Kevin Jensen <kevinjensen1133@yahoo.com>
2021-05-10 11:57:42 -0700

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+export default function Index() {
+
+    return (
+        <div>
+            <h1>
+                Hello Nadine Happy Belated Mothers Day Now Lets Sell Some Damn Shirts!
+            </h1>
+            <p>Love,<br/>Your emotionally abusive son</p>
+        </div>
+    )
+}
